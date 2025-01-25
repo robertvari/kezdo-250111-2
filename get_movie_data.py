d3b6fe9105334977385ffad747d34bf2f4254204
Robert Vari <mail.robertvari@gmail.com>
2025-01-25 17:55:29 +0100

    update

--- get_movie_data.py
+++ get_movie_data.py
@@ -4,12 +4,23 @@ import tmdbsimple as tmdb
 tmdb.API_KEY = "83cbec0139273280b9a3f8ebc9e35ca9"
 tmdb.REQUESTS_TIMEOUT = 5
 
-def main(page_number):
+def main():
+    os.system("cls")
+    print("-"*50, "THE MOVIE DATABASE", "-"*50)
+    
+    page_number = int( input("Give me the page number: ") )
     movie_list = get_movies(page_number)
     print_movies(movie_list)
-    save_movies(movie_list)
 
-def get_movies(page_number):
+    response = input("Do you want to save this list? (y/n)")
+    if response == "y":
+        save_movies(movie_list)
+    
+    response = input("Do you want to run another request? (y/n)")
+    if response == "y":
+        main()
+
+def get_movies(page_number: int):
     movies = tmdb.Movies()
     return movies.popular(page=page_number)["results"]
 
@@ -18,7 +29,9 @@ def print_movies(movie_list):
         print(movie["title"], movie["vote_average"])
 
 def save_movies(movie_list):
-    pass
+    with open("movie_database.json", "w") as f:
+        json.dump(movie_list, f)
 
+    print("Movie database saved.")
 
-main(page_number=100)
+main()
