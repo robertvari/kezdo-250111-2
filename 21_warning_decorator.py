b0cb2f8c1def0037d8d50e65ee8cb6c625c694f9
Robert Vari <mail.robertvari@gmail.com>
2025-01-26 17:14:16 +0100

    update

--- 21_warning_decorator.py
+++ 21_warning_decorator.py
@@ -1,6 +1,7 @@
 import time, random
+from my_functions.decorators import temp_warning
 
-
+@temp_warning
 def get_sensor_data():
     temp = random.randint(100, 2000)
     print(f"Temperature: {temp}")
