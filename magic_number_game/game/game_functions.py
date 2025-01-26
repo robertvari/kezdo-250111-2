dab605591393484a2c022fc1b046b837c09ebb8d
Robert Vari <mail.robertvari@gmail.com>
2025-01-26 17:45:50 +0100

    update

--- magic_number_game/game/game_functions.py
+++ magic_number_game/game/game_functions.py
@@ -1,4 +1,4 @@
-import os
+import os, random
 
 PLAYER_NAME = None
 MIN_NUMBER = 1
@@ -14,19 +14,32 @@ def clear_screen():
 def intro():
     print("-"*50, "MAGIC NUMBERS", "-"*50)
     get_player_name()
-
+    print(f"I have a number between {MIN_NUMBER} and {MAX_NUMBER}. Can you guess it?")
 
 def get_player_name():
     global PLAYER_NAME
-    
+
     response = input("What is your name? ")
     PLAYER_NAME = response
 
 def get_player_number():
-    pass
+    return input("What is your number? ")
 
 def game_loop():
-    pass
+    max_tries = 3
+    print(f"You have {max_tries} tries.")
+    magic_number = str( random.randint(MIN_NUMBER, MAX_NUMBER) )
+    player_number = get_player_number()
+
+    while player_number != magic_number:
+        max_tries -=1
+        if max_tries == 0:
+            exit_game()
+        
+        print(f"Wrong guess. You have {max_tries} tries left.")
+        player_number = get_player_number()
 
 def exit_game():
-    pass
+    clear_screen()
+    print("Sorry, you lost the game :(")
+    exit()
--- magic_number_game/main.py
+++ magic_number_game/main.py
@@ -3,5 +3,6 @@ from game import game_functions as gf
 def main():
     gf.clear_screen()
     gf.intro()
+    gf.game_loop()
 
 main()
