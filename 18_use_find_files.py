b8ebaa8bcd061fe5bb5405ea4ec66f4c4b79f58a
Robert Vari <mail.robertvari@gmail.com>
2025-01-26 16:00:49 +0100

    update

--- 18_use_find_files.py
+++ 18_use_find_files.py
@@ -2,4 +2,8 @@ from my_functions.file_utils import find_files
 
 root_folder = r"C:\Work\Houdini"
 file_list = []
-find_files(root_folder, file_list)
+find_files(root_folder, file_list, filter=".jpg")
+
+for i in file_list:
+    print(i)
+print(f"We found {len(file_list)} files")
