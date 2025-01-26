83d882d54019de8ae817407bc5e85740db11b588
Robert Vari <mail.robertvari@gmail.com>
2025-01-26 14:33:55 +0100

    update

--- 15_libraries.py
+++ 15_libraries.py
@@ -1,2 +1,5 @@
-import my_functions
-print(my_functions)
+from my_functions import math_functions
+
+print(math_functions.add_numbers(10, 5))
+print(math_functions.divide_numbers(10, 5))
+print(math_functions.multiply_numbers(10, 5))
